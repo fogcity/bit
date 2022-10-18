1994d3533d4fa9fd14b2bfd3aca417e34e6ab937
xymzhs <xymzhs@gmail.com>
2022-10-18 18:01:07 +0800

    init : create lib structure

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+
+import { useEffect } from "react";
+import * as Bit from  "../lib";
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+
+root.render(
+  <React.StrictMode>
+   
+  </React.StrictMode>
+);

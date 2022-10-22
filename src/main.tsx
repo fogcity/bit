6d8735f6aef898a707fd326b84e815fb35a1f3db
xymzhs <xymzhs@gmail.com>
2022-10-22 23:23:16 +0800

    docs : update desc

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 import * as Bit from  "../lib";
 const container = document.getElementById("root");
 const root = createRoot(container);
-
+console.log(Bit);
 
 root.render(
   <React.StrictMode>
